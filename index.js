f9c27d9d41851fbe12d282960008c5cf22468920
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-10-02 10:47:04 +0200

    Atelier Node

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Irvin'));
+console.log(chalk.green('Olivier'));
+console.log(chalk.red('Charlotte'));
+console.log(chalk.yellow('Gerald'));    
